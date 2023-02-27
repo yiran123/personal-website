7d704e02818d12b1bda8460d5e39e9aba2e0dd74
yiran123 <yyyiran@hotmail.com>
2023-02-26 20:59:21 -0800

    delete unused code

--- src/components/Next.js
+++ src/components/Next.js
@@ -5,11 +5,11 @@ import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 
 import { Header2 } from "./Header2/Header2";
 
-import Steelcase from "../assets/projects/STEELCASE.pdf";
+// import Steelcase from "../assets/projects/STEELCASE.pdf";
 import SteelcaseMobile from "../assets/projects/Steelcase_mobile.pdf";
 
 export const Next = () => {
-  const [defaultPdfFile] = useState(Steelcase);
+  // const [defaultPdfFile] = useState(Steelcase);
   const [mobilePdfFile] = useState(SteelcaseMobile);
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
