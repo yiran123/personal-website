6d0c9a54b1d21c3a0f3979a1e68b54a8d86ed477
yiran123 <yyyiran@hotmail.com>
2024-02-23 08:59:37 -0800

    iframe mobile bug fix

--- src/components/YoutubeEmbed.js
+++ src/components/YoutubeEmbed.js
@@ -7,8 +7,7 @@ const YoutubeEmbed = ({ embedId }) => (
       width="853"
       height="480"
       src={`https://www.youtube.com/embed/${embedId}`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title="Embedded youtube"
     />
